53c6dbd5284fe29ec09fdb6a92a9b8c0e193e911
Imran Irfan <eggnog@imrans-mbp.lan>
2024-10-21 17:15:33 -0700

    Add receiver job

--- majima_tax_program/prices/prices.go
+++ majima_tax_program/prices/prices.go
@@ -1,10 +1,30 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
-	TaxRate float64
-	InputPrices []float64
-	TaxIncludedPrices []float64
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
 }
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+	for _, price := range job.InputPrices {
+		resultStr := fmt.Sprintf("%f", price)
+		result[resultStr] = price * (1 + job.TaxRate)
+	}
+
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}
+
+/*
 for _, taxRate := range taxRates {
 	var taxIncludedPrices []float64 = make([]float64, len(prices))
 	for priceIndex, price := range prices {
@@ -12,3 +32,4 @@ for _, taxRate := range taxRates {
 	}
 	result[taxRate] = taxIncludedPrices
 }
+*/
