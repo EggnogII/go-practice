5c23fcbdf3f0bed197ec35648699b34054d8d10e
Imran Irfan <imranbirfan@gmail.com>
2024-10-28 14:09:01 -0700

    Add LoadData() Job

--- majima_tax_program/prices/prices.go
+++ majima_tax_program/prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -8,11 +13,56 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
-func (job TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job *TaxIncludedPriceJob) LoadData() {
+	// Open the file
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("An error occured!")
+		fmt.Println(err)
+		return
+	}
+
+	// Read the contents
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	// Return an error if there is an error during read
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Reading content failed")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	prices := make([]float64, len(lines))
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Println("Converting price to float failed")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
+
+}
+
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadData()
+	result := make(map[string]string)
 	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
 		resultStr := fmt.Sprintf("%0.2f", price)
-		result[resultStr] = price * (1 + job.TaxRate)
+		result[resultStr] = fmt.Sprintf("%0.2f", taxIncludedPrice)
 	}
 	fmt.Println(result)
 }
