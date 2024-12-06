d0634acc8a43dab83b0e178138abeb077d73f087
Imran Irfan <imranbirfan@gmail.com>
2024-12-06 15:23:44 -0800

    Update event saving function to return an id. Refactor to preserve prepare command. Also gitignore

--- Rest_Project/models/event.go
+++ Rest_Project/models/event.go
@@ -20,19 +20,22 @@ var events = []Event{}
 func (e Event) Save() error {
 	// Use $n as a safe way to inject values
 	query := `INSERT INTO events(name, description, location, datetime, user_id)
-	VALUES ($1, $2, $3, $4, $5)`
+	VALUES ($1, $2, $3, $4, $5)
+	RETURNING id;`
 	statement, statement_err := db.DB.Prepare(query)
 	if statement_err != nil {
 		return statement_err
 	}
 	defer statement.Close()
-	result, result_err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	var id_num int
+	result_err := statement.QueryRow(e.Name, e.Description, e.Location, e.DateTime, 1).Scan(&id_num)
 	if result_err != nil {
 		return result_err
 	}
-	id, id_err := result.LastInsertId()
-	e.ID = id
-	return id_err
+
+	e.ID = int64(id_num)
+	return nil
 }
 
 func GetAllEvents() ([]Event, error) {
