d34c49cf80c820c3d42531eca4ecbf9e78981e43
Imran Irfan <imranbirfan@gmail.com>
2024-12-06 14:30:47 -0800

    Create psgrs insertion script

--- Rest_Project/models/event.go
+++ Rest_Project/models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest-project/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,9 +17,22 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// later: add it to database
-	events = append(events, e)
+func (e Event) Save() error {
+	// Use $n as a safe way to inject values
+	query := `INSERT INTO events(name, description, location, datetime, user_id)
+	VALUES ($1, $2, $3, $4, $5)`
+	statement, statement_err := db.DB.Prepare(query)
+	if statement_err != nil {
+		return statement_err
+	}
+	defer statement.Close()
+	result, result_err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if result_err != nil {
+		return result_err
+	}
+	id, id_err := result.LastInsertId()
+	e.ID = id
+	return id_err
 }
 
 func GetAllEvents() []Event {
