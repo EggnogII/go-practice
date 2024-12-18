3fc91227d0ef1a7e5bcaa9c7380373d491700475
Imran Irfan <eggnog@imrans-mbp.lan>
2024-12-17 19:46:20 -0800

    Add cancel registration handler func

--- Rest_Project/routes/register.go
+++ Rest_Project/routes/register.go
@@ -33,5 +33,22 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event with supplied ID."})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not cancel registered user for event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "event cancelled successfully"})
 
 }
