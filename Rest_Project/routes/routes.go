245f57ff59fb0db2d9c83e6fcf69a01ad81475b3
Imran Irfan <eggnog@imrans-mbp.lan>
2024-12-17 18:31:30 -0800

    Update routes to use middleware

--- Rest_Project/routes/routes.go
+++ Rest_Project/routes/routes.go
@@ -1,13 +1,20 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/rest-project/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)    //GET, POST, PUT, PATCH, DELETE
 	server.GET("/events/:id", getEvent) // : denotes dynamic path
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	authenticationGroups := server.Group("/")
+	authenticationGroups.Use(middleware.Authenticate)
+	authenticationGroups.POST("/events", createEvent)
+	authenticationGroups.PUT("/events/:id", updateEvent)
+	authenticationGroups.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
